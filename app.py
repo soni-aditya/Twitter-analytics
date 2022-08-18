450b8a4a28ccf90fde0631c06da6387a24a433f5
Aditya Soni <adityasoni5859@gmail.com>
2022-08-18 18:26:04 +0530

    applied realtime analytics and streaming in the dashboard

--- app.py
+++ app.py
@@ -1,75 +1,215 @@
-# from streamlit_autorefresh import st_autorefresh
-# # Run the autorefresh about every 2000 milliseconds (2 seconds) and stop
-# # after it's been refreshed 100 times.
-# count = st_autorefresh(interval=2000, limit=100, key="fizzbuzzcounter")
-import spacy
-from spacy import displacy
+import time
 import streamlit as st
-from config import *
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from db_service import DbService
+import random
+import plotly.express as px
 import pandas as pd
 
-# ----- PAGE CONFIG (title bar)------
-import tweepy
-
+##############  PAGE SETUP
 st.set_page_config(
     page_title="Tweets Analyzer",
+    page_icon="📊",
     layout="wide",
-    initial_sidebar_state="expanded",
 )
-data = []
-sentiments = []
-df = None
-entities = []
-NER = spacy.load("en_core_web_sm")
+##############
 
-def sentiment_scores(sentence):
-    # Create a SentimentIntensityAnalyzer object.
-    sid_obj = SentimentIntensityAnalyzer()
-    sentiment_dict = sid_obj.polarity_scores(sentence)
+dbService = DbService()
+dbService.initConnect()
 
-    # decide sentiment as positive, negative and neutral
-    if sentiment_dict['compound'] >= 0.05:
-        return "Positive"
 
-    elif sentiment_dict['compound'] <= - 0.05:
-        return "Negative"
+def get_data():
+    print(f"New data")
+    return dbService.fetch()
 
-    else:
-        return "Neutral"
 
-def doNER(text):
-    pred = NER(text)
-    result = []
-    for word in pred.ents:
-        result.append((word.text, word.label_))
-    return result
+def increase():
+    global count
+    count += 1
 
-def getData():
-    global data
-    global filter
-    global df
-    print("Came here.....")
-    auth = tweepy.OAuthHandler(API_KEY, API_KEY_SECRET)
-    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-    api = tweepy.API(auth)
-    tweets = api.home_timeline()
-    for tweet in tweets:
-        data.append(tweet.text)
-        sentiments.append(sentiment_scores(tweet.text))
-        entities.append(doNER(tweet.text))
+# dashboard title
+st.title("Real-Time / Live Twitter Analysis")
 
-    df = pd.DataFrame(list(zip(data, sentiments, entities)),
-                      columns=['Tweets', 'Sentiments', 'Entities Keywords'])
+# filters
 
-getData()
-# ---- SIDEBAR ----
-st.sidebar.header("Apply Filters:")
+# new_filter_placeholder = st.empty()
+# new_filter_input = new_filter_placeholder.text_input('Apply new Filter', key=1)
+# reset = st.button('Reset Filters', key=3)
+# if reset:
+#     new_filter_input = new_filter_placeholder.text_input('Apply new Filter', value='', key=2)
+#
+# if new_filter_input:
+#     st.write(new_filter_input)
+# else:
+#     st.write("No Filters", color='red')
+st.write("Showing results for filters: 'India' | 'Inflation' | 'Recession' | 'China' | 'Jobs'")
+# creating a single-element container
+placeholder = st.empty()
 
-filter = st.sidebar.text_input("Enter Filter String")
-st.sidebar.button(label="Apply", on_click=getData)
+# near real-time / live feed simulation
+for seconds in range(200):
+    data = []
+    if seconds % 2 == 0:
+        data = get_data()
+        print(f"Rebuilt----- {len(data)}")
 
-# ----- MAIN -------
-if df is not None:
-    st.table(df)
+    if len(data) > 0:
+        total = len(data) if data is not None else 0
+        likes = 0
+        retweets = 0
+        replies = 0
+        ref = []
+        retweet_list = []
+        like_list = []
+        tweeted_from_list = []
+        sentiments = {}
+        languages = {}
+        named_entities = {}
+        named_entities_num = {}
+        ########
+        recent_texts = []
+        recent_senti = []
+        recent_ref = []
+        recent_source = []
+
+        recent_data = data[-5:] if len(data) > 5 else data
+        for item in recent_data:
+            recent_texts.append(item['text'])
+            recent_senti.append(item['senti'])
+            recent_ref.append(item['ref'])
+            recent_source.append(item['source'])
+
+        recent_df = pd.DataFrame(list(zip(recent_texts, recent_ref, recent_senti, recent_source)),
+                                 columns=['Tweet', 'Type', 'Sentiment', 'Tweeted from'])
+
+        last_50_data = data[-20:] if len(data) > 20 else data
+        for item in last_50_data:
+            for key, value in item['ner'].items():
+                if len(value) > 0:
+                    for val in value:
+                        if val not in named_entities:
+                            named_entities[val] = 1
+                        else:
+                            named_entities[val] += 1
+
+        for item in data:
+            if item['like'] > 0:
+                likes += item['like']
+            if item['retweet'] > 0:
+                retweets += item['retweet']
+            if item['reply'] > 0:
+                replies += item['reply']
+
+            ref.append(item['ref'])
+            retweet_list.append(item['retweet'] if item['retweet'] > 0 else 0)
+            like_list.append(item['like'] if item['like'] > 0 else 0)
+            tweeted_from_list.append(item['source'])
+
+            if item['senti'] not in sentiments:
+                sentiments[item['senti']] = 1
+            else:
+                sentiments[item['senti']] += 1
+
+            if item['lang'] not in languages:
+                languages[item['lang']] = 1
+            else:
+                languages[item['lang']] += 1
+
+            for key, value in item['ner'].items():
+                if len(value) > 0:
+                    if key not in named_entities_num:
+                        named_entities_num[key] = len(value)
+                    else:
+                        named_entities_num[key] += len(value)
+
+        df = pd.DataFrame(list(zip(ref, retweet_list, like_list, tweeted_from_list)),
+                          columns=['References', 'Retweets', 'Likes', 'TweetedFrom'])
+        with placeholder.container():
+            kpi1, kpi2, kpi3, kpi4 = st.columns(4)
+
+            kpi1.metric(
+                label="Total",
+                value=total,
+            )
+
+            kpi2.metric(
+                label="Likes",
+                value=likes,
+            )
+
+            kpi3.metric(
+                label="Retweets",
+                value=retweets,
+                # delta=-round(balance / count_married) * 100,
+            )
+
+            kpi4.metric(
+                label="Replies",
+                value=replies,
+                # delta=-round(balance / count_married) * 100,
+            )
+
+            # Create two columns for charts
+            fig_col1, fig_col2 = st.columns(2)
+            with fig_col1:
+                st.markdown("### Retweets per tweet type")
+                fig = px.density_heatmap(
+                    data_frame=df, y="Retweets", x="References"
+                )
+                st.write(fig)
+
+            with fig_col2:
+                st.markdown("### Recently mentioned")
+                fig2 = px.bar(x=named_entities.keys(),y=named_entities.values())
+                st.write(fig2)
+
+            fig_col3, fig_col4 = st.columns(2)
+            with fig_col3:
+                st.markdown("### Sentiment distribution")
+                fig3 = px.pie(
+                    data_frame=df, values=sentiments.values(), names=sentiments.keys(), hole=.3,
+                    color_discrete_sequence=px.colors.sequential.RdBu
+                )
+                st.write(fig3)
+
+            with fig_col4:
+                st.markdown("### Language distribution")
+                fig4 = px.pie(
+                    data_frame=df, values=languages.values(), names=languages.keys(), hole=.3,
+                    color_discrete_sequence=px.colors.sequential.Aggrnyl
+                )
+                st.write(fig4)
+
+            fig_col5, fig_col6 = st.columns(2)
+            with fig_col5:
+                st.markdown("### Tweet source distribution")
+                fig5 = px.density_heatmap(
+                    data_frame=df, y="Retweets", x="TweetedFrom"
+                )
+                st.write(fig5)
+
+            with fig_col6:
+                st.markdown("### Talked about Subjects")
+                fig6 = px.pie(
+                    data_frame=df, values=named_entities_num.values(), names=named_entities_num.keys(), hole=.3,
+                    color_discrete_sequence=px.colors.sequential.Viridis
+                )
+                st.write(fig6)
+
+            # Recent Tweets
+            st.subheader('Recent tweets:')
+
+            # CSS to inject contained in a string
+            hide_dataframe_row_index = """
+                        <style>
+                        .row_heading.level0 {display:none}
+                        .blank {display:none}
+                        </style>
+                        """
+
+            # Inject CSS with Markdown
+            st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
+            if recent_df is not None:
+                st.table(recent_df)
+            ### sleep
+            time.sleep(5)
---
+++ static_app.py
@@ -0,0 +1,76 @@
+# from streamlit_autorefresh import st_autorefresh
+# # Run the autorefresh about every 2000 milliseconds (2 seconds) and stop
+# # after it's been refreshed 100 times.
+# count = st_autorefresh(interval=2000, limit=100, key="fizzbuzzcounter")
+import spacy
+from spacy import displacy
+import streamlit as st
+from config import *
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import pandas as pd
+
+# ----- PAGE CONFIG (title bar)------
+import tweepy
+
+st.set_page_config(
+    page_title="Tweets Analyzer",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+data = []
+sentiments = []
+df = None
+entities = []
+NER = spacy.load("en_core_web_sm")
+
+def sentiment_scores(sentence):
+    # Create a SentimentIntensityAnalyzer object.
+    sid_obj = SentimentIntensityAnalyzer()
+    sentiment_dict = sid_obj.polarity_scores(sentence)
+
+    # decide sentiment as positive, negative and neutral
+    if sentiment_dict['compound'] >= 0.05:
+        return "Positive"
+
+    elif sentiment_dict['compound'] <= - 0.05:
+        return "Negative"
+
+    else:
+        return "Neutral"
+
+def doNER(text):
+    pred = NER(text)
+    result = []
+    for word in pred.ents:
+        result.append((word.text, word.label_))
+    return result
+
+def getData():
+    global data
+    global filter
+    global df
+    print("Came here.....")
+    auth = tweepy.OAuthHandler(API_KEY, API_KEY_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+    api = tweepy.API(auth)
+    tweets = api.home_timeline()
+    for tweet in tweets:
+        data.append(tweet.text)
+        sentiments.append(sentiment_scores(tweet.text))
+        entities.append(doNER(tweet.text))
+
+    df = pd.DataFrame(list(zip(data, sentiments, entities)),
+                      columns=['Tweets', 'Sentiments', 'Entities Keywords'])
+
+
+getData()
+# ---- SIDEBAR ----
+st.sidebar.header("Apply Filters:")
+
+filter = st.sidebar.text_input("Enter Filter String")
+st.sidebar.button(label="Apply", on_click=getData)
+
+# ----- MAIN -------
+if df is not None:
+    st.table(df)
